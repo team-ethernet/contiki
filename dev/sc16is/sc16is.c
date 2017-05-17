bc317d18a5c4593f67ac35065ab75dc89e5510ed
Robert Olsson <robert@herjulf.se>
2017-05-17 23:07:51 +0200

    I2C UART WIP 	new file:   dev/sc16is/sc16is.c 	new file:   dev/sc16is/sc16is.h

---
+++ dev/sc16is/sc16is.c
@@ -0,0 +1,170 @@
+#include "contiki.h"
+#include <stdio.h>
+#include <string.h>
+#include <dev/i2c.h>
+#include "sc16is.h"
+#include "lib/sensors.h"
+
+int debug = 1;
+
+unsigned int 
+sc16is_gpio_in(void)
+{
+  uint8_t val;  
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_IOSTATE<<3, &val, 1);
+  return val;
+}
+
+void
+sc16is_tx(uint8_t *buf, int len)
+{
+  int i;
+  for(i= 0; i <len; i++) {
+    i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_THR<<3, buf[i]);
+  }
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_THR<<3, '\n');
+}
+
+void
+uart_speed(uint32_t baud)
+{
+  uint32_t div ;
+  uint8_t lcr, val, prescale = 0;
+
+  //baud = 38400;
+  //baud = 115200;
+
+  div = 14745600/(uint32_t) baud;
+  div = div/16;
+
+  if(div > 0xFFFF) {
+    printf("Doing UART div\n");
+    prescale = SC16IS_MCR_CLKSEL_BIT;
+    div = div>>4;
+  }
+
+  if(debug) 
+    printf("prescle=%d div=%lu %lu %lu\n", prescale, div, div/256, div % 256);
+
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, &lcr, 1);
+
+  /* Open the LCR divisors for configuration */
+  val =  SC16IS_LCR_CONF_MODE_B;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, val);
+  
+  /* Enaable enchancfed features */
+  val =  SC16IS_EFR_ENABLE_BIT;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_EFR<<3, val);
+
+  /* LCR to Normal mode */
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, lcr);
+
+  /* Prescaler */
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_MCR<<3, &val, 1);
+  val |= prescale;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_MCR<<3, val);
+
+  /* Open LCR for config */
+  val =  SC16IS_LCR_CONF_MODE_A;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, val);
+
+  /* Divisor */
+  
+  val = div/256;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_DLH<<3, val);
+  
+  val = div % 256;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_DLL<<3, val);
+
+  /* Restore mode */
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, lcr);
+}
+
+int
+sc16is_init(uint8_t mode)
+{
+  uint8_t val;  
+  char buf[4]="hej";
+
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, &val, 1);
+  printf("LCR=0x%02X\n", val);
+
+  /* Reset FIFOs*/
+  val = SC16IS_FCR_RXRESET_BIT | SC16IS_FCR_TXRESET_BIT;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_FCR<<3, val);
+
+  clock_delay_usec(5);
+
+  val = SC16IS_FCR_FIFO_BIT;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_FCR<<3, val);
+
+  /* Enable EFR */
+  val = SC16IS_LCR_CONF_MODE_B;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, val);
+
+  /* Enaable enchancfed features */
+  val =  SC16IS_EFR_ENABLE_BIT;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_EFR<<3, val);
+
+
+  /* Enable TCL/TLR */
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_MCR<<3, &val, 1);
+  val |= SC16IS_MCR_TCRTLR_BIT;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_MCR<<3, val);
+
+#if 1
+  /* Flow control levels */
+  val = SC16IS_TCR_RX_HALT(48) | SC16IS_TCR_RX_RESUME(24);
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_TCR<<3, val);
+#endif
+
+
+  /* Initialize  UART */
+  val = SC16IS_LCR_WORD_LEN_8;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, val);
+  
+#if 0
+  /* Enable RX & TX FIFO */
+  val = SC16IS_EFCR_RXDISABLE_BIT | SC16IS_EFCR_TXDISABLE_BIT;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_EFCR<<3, val);
+#endif  
+
+  //CHANGE INTERRUPTS
+
+  //sc16is7xx_port_write(port, SC16IS7XX_FCR_REG, SC16IS7XX_FCR_FIFO_BIT);
+
+  //i2c_write_mem(SC16IS_ADDR, uint8_t reg, uint8_t value)
+  //i2c_read_mem(SC16IS_ADDR, uint8_t reg, uint8_t buf[], uint8_t bytes)
+
+  uart_speed(115200);
+  printf("GPIO IN=0x%02X\n", sc16is_gpio_in());
+  sc16is_tx(&buf, 4);
+
+  return 1;
+}
+
+
+void
+sc16is_status(void)
+{
+  uint8_t val;  
+  char buf[4]="hej";
+
+  printf("GPIO IN=0x%02X", sc16is_gpio_in());
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, &val, 1);
+  printf(" LCR=0x%02X", val);
+
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_MCR<<3, &val, 1);
+  printf(" MCR=0x%02X", val);
+
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_IIR<<3, &val, 1);
+  printf(" IIR=0x%02X", val);
+
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_RHR<<3, &val, 1);
+  printf(" RHR=0x%02X %c", val, val);
+
+  printf("\n");
+  sc16is_tx(&buf, 4);
+
+}
+      
---
+++ dev/sc16is/sc16is.h
@@ -0,0 +1,147 @@
+#ifndef SC16IS_H
+#define SC16IS_H
+
+#ifdef I2C_SC16IS_CONF_ADDR
+#define I2C_SC16IS_ADDR     I2C_SC16IS_CONF_ADDR
+#else
+#define I2C_SC16IS_ADDR     (0x9A) /* A0 & A1 to GND */
+#endif
+
+#define SC16IS_RHR	  (0x00) /* RX FIFO */
+#define SC16IS_THR	  (0x00) /* TX FIFO */
+#define SC16IS_IER	  (0x01) /* Interrupt enable */
+#define SC16IS_IIR	  (0x02) /* Interrupt Identification */
+#define SC16IS_FCR	  (0x02) /* FIFO control */
+#define SC16IS_LCR	  (0x03) /* Line Control */
+#define SC16IS_MCR	  (0x04) /* Modem Control */
+#define SC16IS_LSR	  (0x05) /* Line Status */
+#define SC16IS_MSR	  (0x06) /* Modem Status */
+#define SC16IS_SPR	  (0x07) /* Scratch Pad */
+#define SC16IS_TXLVL	  (0x08) /* TX FIFO level */
+#define SC16IS_RXLVL	  (0x09) /* RX FIFO level */
+#define SC16IS_IODIR	  (0x0a) /* I/O Direction - only on 75x/76x */
+#define SC16IS_IOSTATE	  (0x0b) /* I/O State - only on 75x/76x */
+#define SC16IS_IOINTENA	  (0x0c) /* I/O Interrupt Enable  - only on 75x/76x */
+#define SC16IS_IOCONTROL  (0x0e) /* I/O Control  - only on 75x/76x */
+#define SC16IS_EFCR	  (0x0f) /* Extra Features Control */
+
+/* TCR/TLR Register set: Only if ((MCR[2] == 1) && (EFR[4] == 1)) */
+#define SC16IS_TCR	  (0x06) /* Transmit control */
+#define SC16IS_TLR	  (0x07) /* Trigger level */
+
+/* Special Register set: Only if ((LCR[7] == 1) && (LCR != 0xBF)) */
+#define SC16IS_DLL	  (0x00) /* Divisor Latch Low */
+#define SC16IS_DLH	  (0x01) /* Divisor Latch High */
+
+/* Enhanced Register set: Only if (LCR == 0xBF) */
+#define SC16IS_EFR	  (0x02) /* Enhanced Features */
+#define SC16IS_XON1	  (0x04) /* Xon1 word */
+#define SC16IS_XON2	  (0x05) /* Xon2 word */
+#define SC16IS_XOFF1	  (0x06) /* Xoff1 word */
+#define SC16IS_XOFF2	  (0x07) /* Xoff2 word */
+
+/* FCR register bits */
+#define SC16IS_FCR_FIFO_BIT	(1 << 0) /* Enable FIFO */
+#define SC16IS_FCR_RXRESET_BIT	(1 << 1) /* Reset RX FIFO */
+#define SC16IS_FCR_TXRESET_BIT	(1 << 2) /* Reset TX FIFO */
+#define SC16IS_FCR_RXLVLL_BIT	(1 << 6) /* RX Trigger level LSB */
+#define SC16IS_FCR_RXLVLH_BIT	(1 << 7) /* RX Trigger level MSB */
+
+/* LCR bits */
+
+/* Bitlen:  00 -> 5,  01 -> 6,  10 -> 7,  11 -> 8 bits */
+#define SC16IS_LCR_LENGTH0_BIT	(1 << 0) 
+#define SC16IS_LCR_LENGTH1_BIT	(1 << 1) 
+#define SC16IS_LCR_STOPLEN_BIT	(1 << 2) 
+/* stopbits:  0 -> 1, 1 -> 1 1/2 or 2 bits */
+#define SC16IS_LCR_PARITY_BIT	(1 << 3) /* Parity bit enable */
+#define SC16IS_LCR_EVENPARITY_BIT  (1 << 4) /* Even parity bit enable */
+#define SC16IS_LCR_FORCEPARITY_BIT  (1 << 5) /* 9-bit multidrop parity */
+#define SC16IS_LCR_TXBREAK_BIT	(1 << 6) /* TX break enable */
+#define SC16IS_LCR_DLAB_BIT	(1 << 7) /* Divisor Latch enable */
+#define SC16IS_LCR_WORD_LEN_5	(0x00)
+#define SC16IS_LCR_WORD_LEN_6	(0x01)
+#define SC16IS_LCR_WORD_LEN_7	(0x02)
+#define SC16IS_LCR_WORD_LEN_8	(0x03)
+#define SC16IS_LCR_CONF_MODE_A	SC16IS_LCR_DLAB_BIT /* Special reg */
+#define SC16IS_LCR_CONF_MODE_B	0xBF                 /* Enhanced reg */
+
+/* MCR  bits */
+#define SC16IS_MCR_DTR_BIT      (1 << 0) /* DTR complement only on 75x/76x   */
+#define SC16IS_MCR_RTS_BIT	(1 << 1) /* RTS complement */
+#define SC16IS_MCR_TCRTLR_BIT	(1 << 2) /* TCR/TLR register enable */
+#define SC16IS_MCR_LOOP_BIT	(1 << 4) /* Enable loopback test mode */
+#define SC16IS_MCR_XONANY_BIT	(1 << 5) /* Enable Xon Any  write enabled if (EFR[4] == 1) */
+#define SC16IS_MCR_IRDA_BIT	(1 << 6) /* Enable IrDA mode  write enabled if (EFR[4] == 1) */
+#define SC16IS_MCR_CLKSEL_BIT	(1 << 7) /* Divide clock by 4  write enabled (EFR[4] == 1)  */
+
+
+
+
+/* EFCR register bits */
+#define SC16IS_EFCR_9BIT_MODE_BIT	(1 << 0) /* Enable 9-bit or Multidrop
+						  * mode (RS485) */
+#define SC16IS_EFCR_RXDISABLE_BIT	(1 << 1) /* Disable receiver */
+#define SC16IS_EFCR_TXDISABLE_BIT	(1 << 2) /* Disable transmitter */
+#define SC16IS_EFCR_AUTO_RS485_BIT	(1 << 4) /* Auto RS485 RTS direction */
+#define SC16IS_EFCR_RTS_INVERT_BIT	(1 << 5) /* RTS output inversion */
+#define SC16IS_EFCR_IRDA_MODE_BIT	(1 << 7) /* IrDA mode
+						  * 0 = rate upto 115.2 kbit/s
+						  *   - Only 750/760
+						  * 1 = rate upto 1.152 Mbit/s
+						  *   - Only 760
+						  */
+
+/* EFR register bits */
+#define SC16IS_EFR_AUTORTS_BIT	(1 << 6) /* Auto RTS flow ctrl enable */
+#define SC16IS_EFR_AUTOCTS_BIT	(1 << 7) /* Auto CTS flow ctrl enable */
+#define SC16IS_EFR_XOFF2_DETECT_BIT	(1 << 5) /* Enable Xoff2 detection */
+#define SC16IS_EFR_ENABLE_BIT	(1 << 4) /* Enable enhanced functions
+						  * and writing to IER[7:4],
+						  * FCR[5:4], MCR[7:5]
+						  */
+#define SC16IS_EFR_SWFLOW3_BIT	(1 << 3) /* SWFLOW bit 3 */
+#define SC16IS_EFR_SWFLOW2_BIT	(1 << 2) /* SWFLOW bit 2
+						  *
+						  * SWFLOW bits 3 & 2 table:
+						  * 00 -> no transmitter flow
+						  *       control
+						  * 01 -> transmitter generates
+						  *       XON2 and XOFF2
+						  * 10 -> transmitter generates
+						  *       XON1 and XOFF1
+						  * 11 -> transmitter generates
+						  *       XON1, XON2, XOFF1 and
+						  *       XOFF2
+						  */
+#define SC16IS_EFR_SWFLOW1_BIT	(1 << 1) /* SWFLOW bit 2 */
+#define SC16IS_EFR_SWFLOW0_BIT	(1 << 0) /* SWFLOW bit 3
+						  *
+						  * SWFLOW bits 3 & 2 table:
+						  * 00 -> no received flow
+						  *       control
+						  * 01 -> receiver compares
+						  *       XON2 and XOFF2
+						  * 10 -> receiver compares
+						  *       XON1 and XOFF1
+						  * 11 -> receiver compares
+						  *       XON1, XON2, XOFF1 and
+						  *       XOFF2
+						  */
+
+
+
+
+/*
+ * TCR register bits
+ * TCR trigger levels are available from 0 to 60 characters with a granularity
+ * of four.
+ * The programmer must program the TCR such that TCR[3:0] > TCR[7:4]. There is
+ * no built-in hardware check to make sure this condition is met. Also, the TCR
+ * must be programmed with this condition before auto RTS or software flow
+ * control is enabled to avoid spurious operation of the device.
+ */
+#define SC16IS_TCR_RX_HALT(words)	((((words) / 4) & 0x0f) << 0)
+#define SC16IS_TCR_RX_RESUME(words)	((((words) / 4) & 0x0f) << 4)
+
+#endif /* SC16IS_H */
