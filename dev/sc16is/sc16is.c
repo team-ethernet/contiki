905df0ba4f27a4f5d8915599de85cfc1d6e20c58
Robert Olsson <robert@herjulf.se>
2017-05-21 21:01:10 +0200

    Working in progress 	modified:   dev/sc16is/sc16is.c 	modified:   dev/sc16is/sc16is.h

--- dev/sc16is/sc16is.c
+++ dev/sc16is/sc16is.c
@@ -1,184 +1,164 @@
+/* Robert Olsson 2017-05-21 KTH */
+
 #include "contiki.h"
 #include <stdio.h>
 #include <string.h>
 #include <dev/i2c.h>
 #include "sc16is.h"
-#include "lib/sensors.h"
 
-int debug = 1;
+static int debug = 1;
+uint32_t sc16is_xtal = 14745600ul;
 
-unsigned int 
-sc16is_gpio_in(void)
+uint8_t
+sc16is_gpio_get(void)
 {
-  uint8_t val;  
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_IOSTATE<<3, &val, 1);
+  uint8_t val;
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_IOSTATE, &val, 1);
   return val;
 }
-
 void
+sc16is_gpio_set(uint8_t set)
+{
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_IOSTATE, set);
+  return;
+}
+uint8_t
 sc16is_tx(uint8_t *buf, int len)
 {
-  int i;
-  for(i= 0; i <len; i++) {
-    i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_THR<<3, buf[i]);
-    if(buf[i] == 0) {
-      i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_THR<<3, '\n');
+  uint8_t i, maxtx;
+
+  /* Read TX FIFO depth */
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_TXLVL, &maxtx, 1);
+
+  for(i = 0; i < len; i++) {
+    if(i < maxtx) {
+      i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_THR, buf[i]);
+    } else {
       break;
     }
   }
+  return i;
 }
+uint8_t
+sc16is_rx(uint8_t *buf, uint8_t maxlen)
+{
+  uint8_t i;
+  uint8_t lsr; /* rx */
 
+  /* i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_RXLVL, &rx, 1); */
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LSR, &lsr, 1);
+
+  for(i = 0; lsr &SC16IS_LSR_DR_BIT; i++) {
+    /* for(i=0; rx != 0; i++) { */
+    if(i == maxlen) {
+      break;
+    }
+    i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_RHR, &buf[i], 1);
+    i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LSR, &lsr, 1);
+    /* i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_RXLVL, &rx, 1); */
+  }
+  return i;
+}
 void
 sc16is_echo_test(void)
 {
-  uint8_t val;
+  uint8_t len, buf[20];
 
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LSR<<3, &val, 1);  
-
-  while(val & SC16IS_LSR_DR_BIT) {
-    i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_RHR<<3, &val, 1);  
-    i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_THR<<3, val);
-    i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LSR<<3, &val, 1);
-  }
+  len = sc16is_rx(buf, sizeof(buf));
+  len = sc16is_tx(buf, len);
 }
-
-void
-uart_speed(uint32_t baud)
+static void
+sc16is_uart_set_speed(uint32_t baud)
 {
-  uint32_t div ;
+  uint32_t div;
   uint8_t lcr, val, prescale = 0;
 
-  div = 14745600/(uint32_t) baud;
-  div = div/16;
+  div = sc16is_xtal / (uint32_t)baud;
+  div = div / 16;
 
   if(div > 0xFFFF) {
-    printf("Doing UART div\n");
     prescale = SC16IS_MCR_CLKSEL_BIT;
-    div = div>>4;
+    div = div >> 4;
   }
 
-  if(debug) 
-    printf("prescale=%d div=%lu %lu %lu\n", prescale, div, div/256, div % 256);
-
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, &lcr, 1);
-
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LCR, &lcr, 1);
   /* Open the LCR divisors for configuration */
-  val =  SC16IS_LCR_CONF_MODE_B;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, val);
-  
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR, SC16IS_LCR_CONF_MODE_B);
   /* Enaable enchancfed features */
-  val =  SC16IS_EFR_ENABLE_BIT;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_EFR<<3, val);
-
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_EFR, SC16IS_EFR_ENABLE_BIT);
   /* LCR to Normal mode */
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, lcr);
-
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR, lcr);
   /* Prescaler */
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_MCR<<3, &val, 1);
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_MCR, &val, 1);
   val |= prescale;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_MCR<<3, val);
-
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_MCR, val);
   /* Open LCR for config */
-  val =  SC16IS_LCR_CONF_MODE_A;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, val);
-
-  /* Divisor */
-  
-  val = div/256;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_DLH<<3, val);
-  
-  val = div % 256;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_DLL<<3, val);
-
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR, SC16IS_LCR_CONF_MODE_A);
+  /* Divisor and reminder */
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_DLH, div >> 8);
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_DLL, (div & 0xFF));
   /* Restore mode */
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, lcr);
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR, lcr);
 }
+static uint8_t
+tx_empty(void)
+{
+  uint8_t lvl, lsr;
 
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_TXLVL, &lvl, 1);
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LSR, &lsr, 1);
+  return (lsr & SC16IS_LSR_THRE_BIT) && !lvl;
+}
 int
-sc16is_init(uint8_t mode)
+sc16is_init(void)
 {
-  uint8_t val;  
-  char buf[4]="hej";
+  uint8_t val;
 
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, &val, 1);
-  printf("LCR=0x%02X\n", val);
-
-  /* Reset FIFOs*/
-  val = SC16IS_FCR_RXRESET_BIT | SC16IS_FCR_TXRESET_BIT;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_FCR<<3, val);
-
+  /*  We use chip FIFO mode  */
+  /* Reset FIFOs */
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_FCR, (SC16IS_FCR_RXRESET_BIT | SC16IS_FCR_TXRESET_BIT));
   clock_delay_usec(5);
-
-  val = SC16IS_FCR_FIFO_BIT;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_FCR<<3, val);
-
+  /* Enable FIFO */
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_FCR, SC16IS_FCR_FIFO_BIT);
   /* Enable EFR */
-  val = SC16IS_LCR_CONF_MODE_B;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, val);
-
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR, SC16IS_LCR_CONF_MODE_B);
   /* Enaable enchancfed features */
-  val =  SC16IS_EFR_ENABLE_BIT;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_EFR<<3, val);
-
-
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_EFR, SC16IS_EFR_ENABLE_BIT);
   /* Enable TCL/TLR */
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_MCR<<3, &val, 1);
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_MCR, &val, 1);
   val |= SC16IS_MCR_TCRTLR_BIT;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_MCR<<3, val);
-
-#if 1
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_MCR, val);
   /* Flow control levels */
-  val = SC16IS_TCR_RX_HALT(48) | SC16IS_TCR_RX_RESUME(24);
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_TCR<<3, val);
-#endif
-
-
-  /* Initialize  UART */
-  val = SC16IS_LCR_WORD_LEN_8;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, val);
-  
-#if 0
-  /* Enable RX & TX FIFO */
-  val = SC16IS_EFCR_RXDISABLE_BIT | SC16IS_EFCR_TXDISABLE_BIT;
-  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_EFCR<<3, val);
-#endif  
-
-  //CHANGE INTERRUPTS
-
-  //sc16is7xx_port_write(port, SC16IS7XX_FCR_REG, SC16IS7XX_FCR_FIFO_BIT);
-
-  //i2c_write_mem(SC16IS_ADDR, uint8_t reg, uint8_t value)
-  //i2c_read_mem(SC16IS_ADDR, uint8_t reg, uint8_t buf[], uint8_t bytes)
-
-  uart_speed(115200);
-   return 1;
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_TCR, (SC16IS_TCR_RX_HALT(48) | SC16IS_TCR_RX_RESUME(24)));
+  /* Initialize UART for 8 bit */
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_LCR, SC16IS_LCR_WORD_LEN_8);
+  /* Enable RX & TX FIFO -- Clear disable bits*/
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_EFCR, &val, 1);
+  val &= ~(SC16IS_EFCR_RXDISABLE_BIT | SC16IS_EFCR_TXDISABLE_BIT);
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_EFCR, val);
+  /* Enable RX, TX, CTS change interrupts */
+  i2c_write_mem(I2C_SC16IS_ADDR, SC16IS_IER, (SC16IS_IER_RDI_BIT | SC16IS_IER_THRI_BIT | SC16IS_IER_CTSI_BIT));
+  sc16is_uart_set_speed(115200);
+  return 1;
 }
-
 void
-sc16is_status(void)
+sc16is_debug_register(void)
 {
-  uint8_t val;  
-  char buf[4]="hej";
+  uint8_t val;
 
-  printf("GPIO IN=0x%02X", sc16is_gpio_in());
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LCR<<3, &val, 1);
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_TXLVL, &val, 1);
+  printf("TXFIFO=%2d", val);
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_RXLVL, &val, 1);
+  printf(" RXFIFO=%2d", val);
+  printf(" GPIO=0x%02X", sc16is_gpio_get());
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LCR, &val, 1);
   printf(" LCR=0x%02X", val);
-
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_MCR<<3, &val, 1);
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_MCR, &val, 1);
   printf(" MCR=0x%02X", val);
-
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_IER<<3, &val, 1);
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_IER, &val, 1);
   printf(" IER=0x%02X", val);
-
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_IIR<<3, &val, 1);
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_IIR, &val, 1);
   printf(" IIR=0x%02X", val);
-
-  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LSR<<3, &val, 1);
-  printf(" LSR=0x%02X %c", val, val);
-
-  printf("\n");
-  //sc16is_tx(&buf, sizeof(buf));
-  sc16is_echo_test();
-
+  i2c_read_mem(I2C_SC16IS_ADDR, SC16IS_LSR, &val, 1);
+  printf(" LSR=0x%02X %c\n", val, val);
 }
-      
