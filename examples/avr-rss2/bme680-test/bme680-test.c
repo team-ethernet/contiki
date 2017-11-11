569eb476be6a4152f4c194a08c1199c6198c5d20
Robert Olsson <robert@herjulf.se>
2017-11-11 21:01:28 +0100

    Adding example for bme680

---
+++ examples/avr-rss2/bme680-test/bme680-test.c
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2015, Copyright Robert Olsson / Radio Sensors AB  
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ *
+ * Author  : Robert Olsson robert@radio-sensors.com
+ * Created : 2017-11-10
+ */
+
+/**
+ * \file
+ *         A simple application showing sensor reading on RSS2 mote
+ */
+
+#include "contiki.h"
+#include "sys/etimer.h"
+#include <stdio.h>
+#include "adc.h"
+#include "i2c.h"
+#include "dev/leds.h"
+#include "dev/bme680/bme680-sensor.h"
+/*---------------------------------------------------------------------------*/
+PROCESS(bme680_test_process, "BME680 test process");
+AUTOSTART_PROCESSES(&bme680_test_process);
+
+static struct etimer et;
+
+static void
+read_values(void)
+{
+
+  if( i2c_probed & I2C_BME280 ) {
+
+    bme680_sensor.value(BME680_SENSOR_TEMP);
+    printf(" T=%5.2f", (double)bme680_mea.t_overscale100 / 100.);
+    printf(" RH=%5.2f", (double)bme680_mea.h_overscale1024 / 1024.);
+    printf(" P=%5.2f", (double)bme680_mea.p);
+    printf(" G=%u", (uint16_t) bme680_mea.g);
+  }
+
+  printf("\n");
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(bme680_test_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  if( i2c_probed & I2C_BME680 ) {
+    SENSORS_ACTIVATE(bme680_sensor);
+  }
+
+  leds_init(); 
+  leds_on(LEDS_RED);
+  leds_on(LEDS_YELLOW);
+
+    etimer_set(&et, CLOCK_SECOND * 5);
+  while(1) {
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+    read_values();
+    etimer_reset(&et);
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
