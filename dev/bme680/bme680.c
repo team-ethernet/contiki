a0b7358349fb36cd46a43dfb353ae20877758552
Robert Olsson <robert@herjulf.se>
2017-11-13 17:41:57 +0100

    modified:   dev/bme680/bme680.c 	modified:   dev/bme680/bme680.h

--- dev/bme680/bme680.c
+++ dev/bme680/bme680.c
@@ -76,103 +76,125 @@ static struct {
   int8_t range_sw_err;
 } cal;
 
-uint8_t buf[BME680_COEFF_ADDR1_LEN+BME680_COEFF_ADDR2_LEN];
+uint8_t buf[BME680_COEFF_ADDR1_LEN + BME680_COEFF_ADDR2_LEN];
 
-uint32_t lookupTable1[16] = { 
-  2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 
-  2126008810, 2147483647, 2130303777, 2147483647, 2147483647, 
-  2143188679, 2136746228, 2147483647, 2126008810, 2147483647, 2147483647 };
+uint32_t lookupTable1[16] = {
+  2147483647ul, 2147483647ul, 2147483647ul, 2147483647ul, 2147483647ul,
+  2126008810ul, 2147483647ul, 2130303777ul, 2147483647ul, 2147483647ul,
+  2143188679ul, 2136746228ul, 2147483647ul, 2126008810ul, 2147483647ul, 2147483647ul
+};
 /**Look up table for the possible gas range values */
-uint32_t lookupTable2[16] = { 
-  4096000000ul, 2048000000, 1024000000, 512000000, 255744255, 
-  127110228,    64000000,   32258064, 16016016,    8000000,  
-    4000000,     2000000,    1000000,   500000,     250000, 125000 };
+uint32_t lookupTable2[16] = {
+  4096000000ul, 2048000000ul, 1024000000ul, 512000000ul, 255744255ul,
+  127110228ul, 64000000ul, 32258064ul, 16016016ul, 8000000ul,
+  4000000ul, 2000000ul, 1000000ul, 500000ul, 250000ul, 125000ul
+};
 
-static int32_t 
+static int32_t
 calc_t(uint32_t temp_adc)
 {
   int64_t v1, v2, v3;
   int32_t temp;
-  
-  v1 = ((int32_t) temp_adc / 8) - ((int32_t) cal.t1 * 2);
-  v2 = (v1 * (int32_t) cal.t2) / 2048;
+
+  v1 = ((int32_t)temp_adc / 8) - ((int32_t)cal.t1 * 2);
+  v2 = (v1 * (int32_t)cal.t2) / 2048;
   v3 = ((v1 / 2) * (v1 / 2)) / 4096;
-  v3 = ((v3) * ((int32_t) cal.t3 * 16)) / 16384;
-  cal.t_fine = (int32_t) (v2 + v3);
-  //temp = (int32_t) (v2 + v3);
-  temp = (int16_t) (((cal.t_fine * 5) + 128) / 256);
+  v3 = ((v3) * ((int32_t)cal.t3 * 16)) / 16384;
+  cal.t_fine = (int32_t)(v2 + v3);
+  /* temp = (int32_t) (v2 + v3); */
+  temp = (int16_t)(((cal.t_fine * 5) + 128) / 256);
   return temp;
 }
-
-static uint32_t 
+static uint32_t
 calc_p(uint32_t pres_adc)
 {
   int64_t v1, v2, v3;
   int32_t calc_pres;
-	
-  v1 = (((int32_t) cal.t_fine) / 2) - 64000;
-  v2 = ((v1 / 4) * (v1 / 4)) / 2048;
-  v2 = ((v2) * (int32_t) cal.p6) / 4;
-  v2 = v2 + ((v1 * (int32_t) cal.p5) * 2);
-  v2 = (v2 / 4) + ((int32_t) cal.p4 * 65536);
-  v1 = ((v1 / 4) * (v1 / 4)) / 8192;
-  v1 = (((v1) * ((int32_t) cal.p3 * 32)) / 8) + (((int32_t) cal.p2 * v1) / 2);
-  v1 = v1 / 262144;
-  v1 = ((32768 + v1) * (int32_t) cal.p1) / 32768;
-  calc_pres = (int32_t) (1048576 - pres_adc);
-  calc_pres = (int32_t) ((calc_pres - (v2 / 4096)) * (3125));
-  calc_pres = ((calc_pres / v1) * 2);
-  v1 = ((int32_t) cal.p9 * (int32_t) (((calc_pres / 8) * (calc_pres / 8)) / 8192)) / 4096;
-  v2 = ((int32_t) (calc_pres / 4) * (int32_t) cal.p8) / 8192;
-  v3 = ((int32_t) (calc_pres / 256) * (int32_t) (calc_pres / 256) * (int32_t) (calc_pres / 256)
-	  * (int32_t) cal.p10) / 131072;
-  calc_pres = (int32_t) (calc_pres) + ((v1 + v2 + v3 + ((int32_t) cal.p7 * 128)) / 16);
-  
-  return (uint32_t) calc_pres;
-}
 
-static uint32_t 
+  v1 = (((int32_t)cal.t_fine) / 2) - 64000;
+  v2 = ((v1 / 4) * (v1 / 4)) / 2048;
+  v2 = ((v2) * (int32_t)cal.p6) / 4;
+  v2 = v2 + ((v1 * (int32_t)cal.p5) * 2);
+  v2 = (v2 / 4) + ((int32_t)cal.p4 * 65536);
+  v1 = ((v1 / 4) * (v1 / 4)) / 8192;
+  v1 = (((v1) * ((int32_t)cal.p3 * 32)) / 8) + (((int32_t)cal.p2 * v1) / 2);
+  v1 = v1 / 262144;
+  v1 = ((32768 + v1) * (int32_t)cal.p1) / 32768;
+  calc_pres = (int32_t)(1048576 - pres_adc);
+  calc_pres = (int32_t)((calc_pres - (v2 / 4096)) * (3125));
+  calc_pres = ((calc_pres / v1) * 2);
+  v1 = ((int32_t)cal.p9 * (int32_t)(((calc_pres / 8) * (calc_pres / 8)) / 8192)) / 4096;
+  v2 = ((int32_t)(calc_pres / 4) * (int32_t)cal.p8) / 8192;
+  v3 = ((int32_t)(calc_pres / 256) * (int32_t)(calc_pres / 256) * (int32_t)(calc_pres / 256)
+        * (int32_t)cal.p10) / 131072;
+  calc_pres = (int32_t)(calc_pres) + ((v1 + v2 + v3 + ((int32_t)cal.p7 * 128)) / 16);
+
+  return (uint32_t)calc_pres;
+}
+static uint32_t
 calc_h(uint16_t hum_adc)
 {
   int32_t v1, v2, v3, v4, v5, v6;
   int32_t temp_scaled, calc_hum;
-	
-  temp_scaled = (((int32_t) cal.t_fine * 5) + 128) / 256;
-  v1 = (int32_t) (hum_adc - ((int32_t) ((int32_t) cal.h1 * 16)))
-    - (((temp_scaled * (int32_t) cal.h3) / ((int32_t) 100)) / 2);
-  v2 = ((int32_t) cal.h2
-	  * (((temp_scaled * (int32_t) cal.h4) / ((int32_t) 100))
-	     + (((temp_scaled * ((temp_scaled * (int32_t) cal.h5) / ((int32_t) 100))) / 64)
-		/ ((int32_t) 100)) + (int32_t) (1 * 16384))) / 1024;
+
+  temp_scaled = (((int32_t)cal.t_fine * 5) + 128) / 256;
+  v1 = (int32_t)(hum_adc - ((int32_t)((int32_t)cal.h1 * 16)))
+    - (((temp_scaled * (int32_t)cal.h3) / ((int32_t)100)) / 2);
+  v2 = ((int32_t)cal.h2
+        * (((temp_scaled * (int32_t)cal.h4) / ((int32_t)100))
+           + (((temp_scaled * ((temp_scaled * (int32_t)cal.h5) / ((int32_t)100))) / 64)
+              / ((int32_t)100)) + (int32_t)(1 * 16384))) / 1024;
   v3 = v1 * v2;
-  v4 = (int32_t) cal.h6 * 128;
-  v4 = ((v4) + ((temp_scaled * (int32_t) cal.h7) / ((int32_t) 100))) / 16;
+  v4 = (int32_t)cal.h6 * 128;
+  v4 = ((v4) + ((temp_scaled * (int32_t)cal.h7) / ((int32_t)100))) / 16;
   v5 = ((v3 / 16384) * (v3 / 16384)) / 1024;
   v6 = (v4 * v5) / 2;
-  calc_hum = (((v3 + v6) / 1024) * ((int32_t) 1000)) / 4096;
-  
-  if (calc_hum > 100000) /* Cap at 100%rH */
-    calc_hum = 100000;
-  else if (calc_hum < 0)
-    calc_hum = 0;
-  return (uint32_t) calc_hum;
-}
+  calc_hum = (((v3 + v6) / 1024) * ((int32_t)1000)) / 4096;
 
-static uint32_t calc_gas_res(uint16_t gas_res_adc, uint8_t gas_range)
+  if(calc_hum > 100000) { /* Cap at 100%rH */
+    calc_hum = 100000;
+  } else if(calc_hum < 0) {
+    calc_hum = 0;
+  }
+  return (uint32_t)calc_hum;
+}
+static uint32_t
+calc_gas_res(uint16_t gas_res_adc, uint8_t gas_range)
 {
   int64_t v1;
   uint64_t v2;
   int64_t v3;
   uint32_t calc_gas_res;
 
-  v1 = (int64_t) ((1340 + (5 * (int64_t) cal.range_sw_err)) * ((int64_t) lookupTable1[gas_range])) / 65536;
-  v2 = (((int64_t) ((int64_t) gas_res_adc * 32768) - (int64_t) (16777216)) + v1);
-  v3 = (((int64_t) lookupTable2[gas_range] * (int64_t) v1) / 512);
-  calc_gas_res = (uint32_t) ((v3 + ((int64_t) v2 / 2)) / (int64_t) v2);
+  v1 = (int64_t)((1340 + (5 * (int64_t)cal.range_sw_err)) * ((int64_t)lookupTable1[gas_range])) / 65536;
+  v2 = (((int64_t)((int64_t)gas_res_adc * 32768) - (int64_t)(16777216)) + v1);
+  v3 = (((int64_t)lookupTable2[gas_range] * (int64_t)v1) / 512);
+  calc_gas_res = (uint32_t)((v3 + ((int64_t)v2 / 2)) / (int64_t)v2);
   return calc_gas_res;
 }
+double const t1[16] = {
+  1., 1., 1., 1., 1., 0.99, 1., 0.992,
+  1., 1., 0.998, 0.995, 1., 0.99, 1., 1.
+};
 
-static uint8_t 
+double const t2[16] = {
+  8000000., 4000000., 2000000., 1000000., 499500.4995, 248262.1648, 125000, 63004.03226, 31281.28128,
+  15625., 7812.5, 3906.25, 1953.125, 976.5625, 488.28125, 244.140625
+};
+
+static double
+calc_gas_res_d(uint16_t gas_r, uint8_t gas_range)
+{
+  double v1, v2, calc_gas_res = 0;
+
+  v1 = (double)(1340. + (5.0 * (double)cal.range_sw_err)) * (double)t1[gas_range];
+  v2 = (double)(gas_r - 512.0 + v1);
+  if(v2) {
+    calc_gas_res = (double)(v1 * (double)t2[gas_range]) / (double)v2;
+  }
+  return calc_gas_res;
+}
+static uint8_t
 calc_heater_res(uint16_t temp)
 {
   uint8_t heatr_res;
@@ -180,42 +202,41 @@ calc_heater_res(uint16_t temp)
   int32_t heatr_res_x100;
   int8_t atemp;  /* For heater calc */
 
-  if (temp < 200) /* Cal temperature */
+  if(temp < 200) { /* Cal temperature */
     temp = 200;
-  else if (temp > 400)
+  } else if(temp > 400) {
     temp = 400;
+  }
 
   /* Ambient temperature in degree C */
-  atemp = bme680_mea.t_overscale100/100;
-  v1 = (((int32_t) atemp * cal.gh3) / 1000) * 256;
+  atemp = bme680_mea.t_overscale100 / 100;
+  v1 = (((int32_t)atemp * cal.gh3) / 1000) * 256;
   v2 = (cal.gh1 + 784) * (((((cal.gh2 + 154009) * temp * 5) / 100) + 3276800) / 10);
   v3 = v1 + (v2 / 2);
   v4 = (v3 / (cal.res_heat_range + 4));
   v5 = (131 * cal.res_heat_val) + 65536;
-  heatr_res_x100 = (int32_t) (((v4 / v5) - 250) * 34);
-  heatr_res = (uint8_t) ((heatr_res_x100 + 50) / 100);
+  heatr_res_x100 = (int32_t)(((v4 / v5) - 250) * 34);
+  heatr_res = (uint8_t)((heatr_res_x100 + 50) / 100);
   return heatr_res;
 }
-
 /*   calculate the Heat duration value. */
-static uint8_t 
+static uint8_t
 calc_heater_dur(uint16_t dur)
 {
   uint8_t factor = 0;
   uint8_t durval;
 
-  if (dur >= 0xfc0) {
+  if(dur >= 0xfc0) {
     durval = 0xff; /* Max duration*/
   } else {
-    while (dur > 0x3F) {
+    while(dur > 0x3F) {
       dur = dur / 4;
       factor += 1;
     }
-    durval = (uint8_t) (dur + (factor * 64));
+    durval = (uint8_t)(dur + (factor * 64));
   }
   return durval;
 }
-
 uint8_t
 bme680_init(void)
 {
@@ -240,95 +261,102 @@ bme680_init(void)
   /* Burst read of all calibration part 1 */
   bme680_arch_i2c_read_mem(BME680_ADDR, BME680_COEFF_ADDR1, buf, BME680_COEFF_ADDR1_LEN);
   /* Burst read of all calibration part 2 */
-  bme680_arch_i2c_read_mem(BME680_ADDR, BME680_COEFF_ADDR2, &buf[BME680_COEFF_ADDR1_LEN], 
-			   BME680_COEFF_ADDR2_LEN);
+  bme680_arch_i2c_read_mem(BME680_ADDR, BME680_COEFF_ADDR2, &buf[BME680_COEFF_ADDR1_LEN],
+                           BME680_COEFF_ADDR2_LEN);
 
   /* Temp coeff */
   cal.t1 = ((uint16_t)buf[BME680_T1_MSB_REG] << 8) | buf[BME680_T1_LSB_REG];
   cal.t2 = ((int16_t)buf[BME680_T2_MSB_REG] << 8) | buf[BME680_T2_LSB_REG];
   cal.t3 = (uint16_t)buf[BME680_T3_REG];
   /* Pressure coeff */
-  cal.p1 = (uint16_t) (buf[BME680_P1_MSB_REG]<<8) | buf[BME680_P1_LSB_REG];
-  cal.p2 = (int16_t) (buf[BME680_P2_MSB_REG]<<8) | buf[BME680_P2_LSB_REG];
-  cal.p3 = (int8_t) buf[BME680_P3_REG];
-  cal.p4 = (int16_t) (buf[BME680_P4_MSB_REG]<<8) | buf[BME680_P4_LSB_REG];
-  cal.p5 = (int16_t) (buf[BME680_P5_MSB_REG]<<8) | buf[BME680_P5_LSB_REG];
-  cal.p6 = (int8_t) (buf[BME680_P6_REG]);
-  cal.p7 = (int8_t) (buf[BME680_P7_REG]);
-  cal.p8 = (int16_t) (buf[BME680_P8_MSB_REG]<<8) | buf[BME680_P8_LSB_REG];
-  cal.p9 = (int16_t) (buf[BME680_P9_MSB_REG]<<8) | buf[BME680_P9_LSB_REG];
-  cal.p10 = (uint8_t) (buf[BME680_P10_REG]);
+  cal.p1 = (uint16_t)(buf[BME680_P1_MSB_REG] << 8) | buf[BME680_P1_LSB_REG];
+  cal.p2 = (int16_t)(buf[BME680_P2_MSB_REG] << 8) | buf[BME680_P2_LSB_REG];
+  cal.p3 = (int8_t)buf[BME680_P3_REG];
+  cal.p4 = (int16_t)(buf[BME680_P4_MSB_REG] << 8) | buf[BME680_P4_LSB_REG];
+  cal.p5 = (int16_t)(buf[BME680_P5_MSB_REG] << 8) | buf[BME680_P5_LSB_REG];
+  cal.p6 = (int8_t)(buf[BME680_P6_REG]);
+  cal.p7 = (int8_t)(buf[BME680_P7_REG]);
+  cal.p8 = (int16_t)(buf[BME680_P8_MSB_REG] << 8) | buf[BME680_P8_LSB_REG];
+  cal.p9 = (int16_t)(buf[BME680_P9_MSB_REG] << 8) | buf[BME680_P9_LSB_REG];
+  cal.p10 = (uint8_t)(buf[BME680_P10_REG]);
 
   /* RH coeff */
-  cal.h1 = (uint16_t) (((uint16_t) buf[BME680_H1_MSB_REG] << BME680_HUM_REG_SHIFT_VAL) | 
-		       (buf[BME680_H1_LSB_REG] & BME680_BIT_H1_DATA_MSK));
-  cal.h2 = (uint16_t) (((uint16_t) buf[BME680_H2_MSB_REG] << BME680_HUM_REG_SHIFT_VAL) | 
-		       ((buf[BME680_H2_LSB_REG]) >> BME680_HUM_REG_SHIFT_VAL));
-  cal.h3 = (int8_t) buf[BME680_H3_REG];
-  cal.h4 = (int8_t) buf[BME680_H4_REG];
-  cal.h5 = (int8_t) buf[BME680_H5_REG];
-  cal.h6 = (uint8_t) buf[BME680_H6_REG];
-  cal.h7 = (int8_t) buf[BME680_H7_REG];
+  cal.h1 = (uint16_t)(((uint16_t)buf[BME680_H1_MSB_REG] << BME680_HUM_REG_SHIFT_VAL) |
+                      (buf[BME680_H1_LSB_REG] & BME680_BIT_H1_DATA_MSK));
+  cal.h2 = (uint16_t)(((uint16_t)buf[BME680_H2_MSB_REG] << BME680_HUM_REG_SHIFT_VAL) |
+                      ((buf[BME680_H2_LSB_REG]) >> BME680_HUM_REG_SHIFT_VAL));
+  cal.h3 = (int8_t)buf[BME680_H3_REG];
+  cal.h4 = (int8_t)buf[BME680_H4_REG];
+  cal.h5 = (int8_t)buf[BME680_H5_REG];
+  cal.h6 = (uint8_t)buf[BME680_H6_REG];
+  cal.h7 = (int8_t)buf[BME680_H7_REG];
 
   /* Heater coeff */
-  cal.gh1 = (int8_t) buf[BME680_GH1_REG];
-  cal.gh2 = (int16_t) buf[BME680_GH2_MSB_REG]<<8 | buf[BME680_GH2_LSB_REG];
-  cal.gh3 = (int8_t) buf[BME680_GH3_REG];
+  cal.gh1 = (int8_t)buf[BME680_GH1_REG];
+  cal.gh2 = (int16_t)buf[BME680_GH2_MSB_REG] << 8 | buf[BME680_GH2_LSB_REG];
+  cal.gh3 = (int8_t)buf[BME680_GH3_REG];
 
   bme680_arch_i2c_read_mem(BME680_ADDR, BME680_ADDR_RES_HEAT_RANGE_ADDR, &tmp, 1);
-  cal.res_heat_range = ( tmp & BME680_RHRANGE_MSK) / 16;
+  cal.res_heat_range = (tmp & BME680_RHRANGE_MSK) / 16;
   bme680_arch_i2c_read_mem(BME680_ADDR, BME680_ADDR_RES_HEAT_VAL_ADDR, &tmp, 1);
   cal.res_heat_val = tmp;
+
   bme680_arch_i2c_read_mem(BME680_ADDR, BME680_ADDR_RANGE_SW_ERR_ADDR, &tmp, 1);
-  cal.range_sw_err = ( tmp & BME680_RSERROR_MSK) / 16;
+  cal.range_sw_err = (tmp & BME680_RSERROR_MSK) / 16;
+
   return 1;
 }
-
 void
 bme680_read(void)
 {
   uint32_t ut, up;
   uint16_t uh, adc_gas_res;
   uint8_t gas_range;
-  uint16_t l1;
   uint8_t ght, ghd;
-  uint8_t sleep;
+  uint8_t sleep, reg;
   uint16_t i;
+
   memset(buf, 0, sizeof(buf));
 
   ut = uh = up = 0;
-  l1 = 0;
-  
-  /* 0.5 ms -- no filter -- no SPI */
-  bme680_arch_i2c_write_mem(BME680_ADDR, BME680_CONFIG, 0x00); 
-  
-  // GAS heater off 
-  //bme680_arch_i2c_write_mem(BME680_ADDR, BME680_GAS_0, 0x04);
 
-  //ght = calc_heater_res(320);
+  /* 0.5 ms -- no filter -- no SPI */
+  bme680_arch_i2c_write_mem(BME680_ADDR, BME680_CONFIG, 0x00);
+
+  bme680_mea.osrs_t = 1;
+  bme680_mea.osrs_p = 5; /* 16 ggr * / */
+  bme680_mea.osrs_h = 1;
+
+  /* GAS heater off */
+  /* bme680_arch_i2c_write_mem(BME680_ADDR, BME680_GAS_0, 0x04); */
+
+  /* ght = calc_heater_res(320); */
   ght = calc_heater_res(300);
   ghd = calc_heater_dur(100);
-  printf("gdt=0x%02x, ghd=0x%02x", ght, ghd);
+  /* printf("gdt=0x%02x, ghd=0x%02x", ght, ghd); */
   bme680_arch_i2c_write_mem(BME680_ADDR, BME680_GAS_WAIT_0, ghd);
   bme680_arch_i2c_write_mem(BME680_ADDR, BME680_RES_HEAT_0, ght);
-  // Select the heater  0 above start 0x10 run_gas
+  /* Select the heater  0 above start 0x10 run_gas */
   bme680_arch_i2c_write_mem(BME680_ADDR, BME680_GAS_1, 0x10);
 
   /* Gas  heater off  */
-  // bme680_arch_i2c_write_mem(BME680_ADDR, BME680_GAS_0, 0x00);
+  /* bme680_arch_i2c_write_mem(BME680_ADDR, BME680_GAS_0, 0x00); */
 
-  /* Humidity oversampling *1 */
-  bme680_arch_i2c_write_mem(BME680_ADDR, BME680_CNTL_HUM, 0x01);
+  /* Humidity oversampling SPI int off */
+  bme680_arch_i2c_write_mem(BME680_ADDR, BME680_CNTL_HUM, bme680_mea.osrs_h);
+
   /* 00100101 Temp and P oversampling * 1 + Trigger FORCE MODE */
-  bme680_arch_i2c_write_mem(BME680_ADDR, BME680_CNTL_MEAS, 0x25);
+
+  reg = (bme680_mea.osrs_p << 5) | (bme680_mea.osrs_t << 2) | 0x01;
+  bme680_arch_i2c_write_mem(BME680_ADDR, BME680_CNTL_MEAS, reg);
 
   /* Wait to get into sleep mode == measurement done */
   for(i = 0; i < BME280_MAX_WAIT; i++) {
     bme680_arch_i2c_read_mem(BME680_ADDR, BME680_MEAS_STATUS_0, &sleep, 1);
-    if(sleep & 0x80) { /* New data */
+    if(sleep& 0x80) {  /* New data */
       break;
     } else {
-      clock_delay_usec(1000); // 1 mS
+      clock_delay_usec(1000); /* 1 mS */
     }
   }
   if(i == BME280_MAX_WAIT) {
@@ -336,39 +364,40 @@ bme680_read(void)
     return; /* error  wait*/
   }
 
-  l1 = i;
-
   /* Burst read of measurements */
-  bme680_arch_i2c_read_mem(BME680_ADDR, BME680_FIELD0_ADDR , buf, BME680_FIELD_LENGTH);
-  //cal.status = buff[0] & BME680_NEW_DATA_MSK;
-  //cal.gas_index = buff[0] & BME680_GAS_INDEX_MSK;
-  //cal.>meas_index = buff[1];
+  bme680_arch_i2c_read_mem(BME680_ADDR, BME680_FIELD0_ADDR, buf, BME680_FIELD_LENGTH);
+  /* cal.status = buff[0] & BME680_NEW_DATA_MSK; */
+  /* cal.gas_index = buff[0] & BME680_GAS_INDEX_MSK; */
+  /* cal.>meas_index = buff[1]; */
 
-  if( buf[14] & BME680_GASM_VALID_MSK)
-    printf(" GAS-OK ");
-  if( buf[14] & BME680_HEAT_STAB_MSK)
-    printf(" HEAT-OK ");
-  printf(" GAS-IDX=%x ", sleep & 0x0f);
+  if(!(buf[14] & BME680_GASM_VALID_MSK)) {
+    printf(" GAS-ERR ");
+  }
+  if(!(buf[14] & BME680_HEAT_STAB_MSK)) {
+    printf(" HEAT-ERR ");
+  }
+
+  /* printf(" GAS-IDX=%x ", sleep & 0x0f); */
 
   /* Set sleep mode */
-  bme680_arch_i2c_write_mem(BME680_ADDR, BME680_CNTL_MEAS, 0x0);
+  reg = (bme680_mea.osrs_p << 5) | (bme680_mea.osrs_t << 2) | 0x00;
+  bme680_arch_i2c_write_mem(BME680_ADDR, BME680_CNTL_MEAS, reg);
 
   /* read the raw data from the sensor */
-  up = (uint32_t) (((uint32_t) buf[2] * 4096) | ((uint32_t) buf[3] * 16) | ((uint32_t) buf[4] / 16));
-  ut = (uint32_t) (((uint32_t) buf[5] * 4096) | ((uint32_t) buf[6] * 16) | ((uint32_t) buf[7] / 16));
-  uh = (uint16_t) (((uint32_t) buf[8] * 256) | (uint32_t) buf[9]);
+  up = (uint32_t)(((uint32_t)buf[2] * 4096) | ((uint32_t)buf[3] * 16) | ((uint32_t)buf[4] / 16));
+  ut = (uint32_t)(((uint32_t)buf[5] * 4096) | ((uint32_t)buf[6] * 16) | ((uint32_t)buf[7] / 16));
+  uh = (uint16_t)(((uint32_t)buf[8] * 256) | (uint32_t)buf[9]);
   bme680_mea.t_overscale100 = calc_t(ut);
   bme680_mea.h_overscale1024 = calc_h(uh);
   bme680_mea.p = calc_p(up);
-  adc_gas_res = (uint16_t) ((uint32_t) buf[13] * 4 | (((uint32_t) buf[14]) / 64));
+  adc_gas_res = ((uint16_t)buf[13] << 2) | (((uint16_t)buf[14]) / 64);
   gas_range = buf[14] & BME680_GAS_RANGE_MSK;
+
   bme680_mea.g = calc_gas_res(adc_gas_res, gas_range);
+  bme680_mea.gd = calc_gas_res_d(adc_gas_res, gas_range);
 
-  printf(" GAS %d %d l1=%u", buf[14] , BME680_GAS_RANGE_MSK, l1);
-
+  /* printf(" GAS %d %d l1=%u", buf[14] , BME680_GAS_RANGE_MSK, l1); */
   /* Set to sleep */
-  bme680_arch_i2c_write_mem(BME680_ADDR, BME680_CNTL_MEAS, 0x0);
-
-  printf(" gas_range=%u adc_gas_res=%u l1=%u ", gas_range, adc_gas_res, l1);
+  printf(" sw_err=%d gas_range=%u adc_gas_res=%u  ", cal.range_sw_err, gas_range, adc_gas_res);
   return;
 }
--- dev/bme680/bme680.h
+++ dev/bme680/bme680.h
@@ -51,31 +51,31 @@ void bme680_read(void);
 #define BME680_ADDR              (0x77 << 1) /* Alternative 0x76 */
 #endif
 
-#define BME680_COEFF_ADDR1_LEN		25
-#define BME680_COEFF_ADDR2_LEN		16
-#define BME680_FIELD_ADDR_OFFSET	17
+#define BME680_COEFF_ADDR1_LEN    25
+#define BME680_COEFF_ADDR2_LEN    16
+#define BME680_FIELD_ADDR_OFFSET  17
 
-#define BME680_ADDR_RES_HEAT_VAL_ADDR	0x00
-#define BME680_ADDR_RES_HEAT_RANGE_ADDR	0x02
-#define BME680_ADDR_RANGE_SW_ERR_ADDR	0x04
-#define BME680_ADDR_SENS_CONF_START	0x5A
-#define BME680_ADDR_GAS_CONF_START	0x64
-#define BME680_FIELD0_ADDR		0x1d
-#define BME680_RES_HEAT0_ADDR		0x5a
-#define BME680_GAS_WAIT0_ADDR		0x64
+#define BME680_ADDR_RES_HEAT_VAL_ADDR 0x00
+#define BME680_ADDR_RES_HEAT_RANGE_ADDR 0x02
+#define BME680_ADDR_RANGE_SW_ERR_ADDR 0x04
+#define BME680_ADDR_SENS_CONF_START 0x5A
+#define BME680_ADDR_GAS_CONF_START  0x64
+#define BME680_FIELD0_ADDR    0x1d
+#define BME680_RES_HEAT0_ADDR   0x5a
+#define BME680_GAS_WAIT0_ADDR   0x64
 
-#define BME680_GAS_RANGE_MSK	        0x0f
+#define BME680_GAS_RANGE_MSK          0x0f
 
-#define BME680_CONF_HEAT_CTRL_ADDR		0x70
-#define BME680_CONF_ODR_RUN_GAS_NBC_ADDR	0x71
-#define BME680_CONF_OS_H_ADDR			0x72
-#define BME680_MEM_PAGE_ADDR			0xf3
-#define BME680_CONF_T_P_MODE_ADDR		0x74
-#define BME680_CONF_ODR_FILT_ADDR		0x75
+#define BME680_CONF_HEAT_CTRL_ADDR    0x70
+#define BME680_CONF_ODR_RUN_GAS_NBC_ADDR  0x71
+#define BME680_CONF_OS_H_ADDR     0x72
+#define BME680_MEM_PAGE_ADDR      0xf3
+#define BME680_CONF_T_P_MODE_ADDR   0x74
+#define BME680_CONF_ODR_FILT_ADDR   0x75
 #define BME680_COEFF_ADDR1      0x89
-#define BME680_COEFF_ADDR2	0xe1
+#define BME680_COEFF_ADDR2  0xe1
 
-#define BME680_RESET_TIME                10         
+#define BME680_RESET_TIME                10
 
 #define BME680_STATUS                   0x73
 #define BME680_RESET                    0xE0
@@ -103,14 +103,14 @@ void bme680_read(void);
 #define BME680_PRES_MSB                 0x1F
 #define BME680_MEAS_STATUS_0            0x1D
 
-#define BME680_HEAT	        	0x70
-#define BME680_ODR_RUN_GAS_NBC   	0x71
+#define BME680_HEAT           0x70
+#define BME680_ODR_RUN_GAS_NBC    0x71
 
-#define BME680_FIELD_LENGTH		15
-#define BME680_FIELD_ADDR_OFFSET	17
+#define BME680_FIELD_LENGTH   15
+#define BME680_FIELD_ADDR_OFFSET  17
 
-#define BME680_HUM_REG_SHIFT_VAL	4
-#define	BME680_BIT_H1_DATA_MSK	       0x0F
+#define BME680_HUM_REG_SHIFT_VAL  4
+#define BME680_BIT_H1_DATA_MSK         0x0F
 
 /* Diffrent BOSCH chip id's */
 #define BMP085_CHIP_ID                 0x55  /* And also BMP180 */
@@ -119,47 +119,47 @@ void bme680_read(void);
 #define BME680_CHIP_ID                 0x61
 
 /** Array Index to Field data mapping for Calibration Data*/
-#define BME680_T2_LSB_REG	(1)
-#define BME680_T2_MSB_REG	(2)
-#define BME680_T3_REG		(3)
-#define BME680_P1_LSB_REG	(5)
-#define BME680_P1_MSB_REG	(6)
-#define BME680_P2_LSB_REG	(7)
-#define BME680_P2_MSB_REG	(8)
-#define BME680_P3_REG		(9)
-#define BME680_P4_LSB_REG	(11)
-#define BME680_P4_MSB_REG	(12)
-#define BME680_P5_LSB_REG	(13)
-#define BME680_P5_MSB_REG	(14)
-#define BME680_P7_REG		(15)
-#define BME680_P6_REG		(16)
-#define BME680_P8_LSB_REG	(19)
-#define BME680_P8_MSB_REG	(20)
-#define BME680_P9_LSB_REG	(21)
-#define BME680_P9_MSB_REG	(22)
-#define BME680_P10_REG		(23)
-#define BME680_H2_MSB_REG	(25)
-#define BME680_H2_LSB_REG	(26)
-#define BME680_H1_LSB_REG	(26)
-#define BME680_H1_MSB_REG	(27)
-#define BME680_H3_REG		(28)
-#define BME680_H4_REG		(29)
-#define BME680_H5_REG		(30)
-#define BME680_H6_REG		(31)
-#define BME680_H7_REG		(32)
-#define BME680_T1_LSB_REG	(33)
-#define BME680_T1_MSB_REG	(34)
-#define BME680_GH2_LSB_REG	(35)
-#define BME680_GH2_MSB_REG	(36)
-#define BME680_GH1_REG		(37)
-#define BME680_GH3_REG		(38)
+#define BME680_T2_LSB_REG (1)
+#define BME680_T2_MSB_REG (2)
+#define BME680_T3_REG   (3)
+#define BME680_P1_LSB_REG (5)
+#define BME680_P1_MSB_REG (6)
+#define BME680_P2_LSB_REG (7)
+#define BME680_P2_MSB_REG (8)
+#define BME680_P3_REG   (9)
+#define BME680_P4_LSB_REG (11)
+#define BME680_P4_MSB_REG (12)
+#define BME680_P5_LSB_REG (13)
+#define BME680_P5_MSB_REG (14)
+#define BME680_P7_REG   (15)
+#define BME680_P6_REG   (16)
+#define BME680_P8_LSB_REG (19)
+#define BME680_P8_MSB_REG (20)
+#define BME680_P9_LSB_REG (21)
+#define BME680_P9_MSB_REG (22)
+#define BME680_P10_REG    (23)
+#define BME680_H2_MSB_REG (25)
+#define BME680_H2_LSB_REG (26)
+#define BME680_H1_LSB_REG (26)
+#define BME680_H1_MSB_REG (27)
+#define BME680_H3_REG   (28)
+#define BME680_H4_REG   (29)
+#define BME680_H5_REG   (30)
+#define BME680_H6_REG   (31)
+#define BME680_H7_REG   (32)
+#define BME680_T1_LSB_REG (33)
+#define BME680_T1_MSB_REG (34)
+#define BME680_GH2_LSB_REG  (35)
+#define BME680_GH2_MSB_REG  (36)
+#define BME680_GH1_REG    (37)
+#define BME680_GH3_REG    (38)
 
 #define BME280_MAX_WAIT                300 /* ms. Forced mode max wait */
 
-#define BME680_RSERROR_MSK	0xf0
-#define BME680_RHRANGE_MSK	0x30
-#define BME680_GASM_VALID_MSK	0x20
-#define BME680_HEAT_STAB_MSK	0x10
+#define BME680_RSERROR_MSK  0xf0
+#define BME680_RHRANGE_MSK  0x30
+#define BME680_GASM_VALID_MSK 0x20
+#define BME680_HEAT_STAB_MSK  0x10
 
 struct {
   int32_t t_overscale100;
@@ -170,6 +170,33 @@ struct {
   uint32_t p;
 #endif
   uint16_t g;
+  double gd;
+
+  /* Oversampling t h p
+     000 Skipped  out 0x8000
+     001 overs*1
+     010 overs*2
+     011 overs*4
+     100 overs*8
+     101 overs*16
+   */
+
+  uint8_t osrs_t;
+  uint8_t osrs_h;
+  uint8_t osrs_p;
+
+  /* Filter
+     000 0
+     001 1
+     010 3
+     011 7
+     100 15
+     101 31
+     110 63
+     111 127
+   */
+
+  uint8_t ir_filter; /* 3bit, 0-7 Corresponds 0 - 127  fot temp and P */
 } bme680_mea;
 
 #endif /* BME680_H */
