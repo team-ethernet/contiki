3bfdefd97e53ab5854c688453144532d1a5f9f31
Robert Olsson <robert@herjulf.se>
2019-02-18 14:50:21 +0100

    KTH "nornal" MQTT setting 	modified:   examples/avr-rss2/ipv6/mqtt-greeniot/project-conf.h

--- examples/avr-rss2/ipv6/mqtt-greeniot/project-conf.h
+++ examples/avr-rss2/ipv6/mqtt-greeniot/project-conf.h
@@ -82,8 +82,11 @@
 #else	/* KTH configuration: this is a default configuration */
 #define MQTT_DEMO_TOPIC_BASE 	"KTH/avr-rss2"
 #define MQTT_DEMO_BROKER_IP_ADDR "0064:ff9b::c010:7dea"
+//#define MQTT_DEMO_BROKER_IP_ADDR "0064:ff9b::c0a8:0103"
 #define RPL_CONF_DEFAULT_INSTANCE 0x1d
 #define IEEE802154_CONF_PANID 0xFEED
+//#define IEEE802154_CONF_PANID 0xABBA
+//#define CHANNEL_CONF_802_15_4 12
 #define CHANNEL_CONF_802_15_4 25
 #define RPL_CONF_WITH_DAO_ACK 1
 #endif /* TESTBED_UPWIS_CONF */
