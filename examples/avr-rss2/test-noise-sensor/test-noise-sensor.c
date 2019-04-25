579caa74c65ecd5d016b10a68a54a73836b57b79
Andreas <andro.sjodin@gmail.com>
2019-04-25 09:29:59 +0200

    trying struct

--- examples/avr-rss2/test-noise-sensor/test-noise-sensor.c
+++ examples/avr-rss2/test-noise-sensor/test-noise-sensor.c
@@ -30,7 +30,7 @@ for(i=0; i < 15; i++){
 
   printf(" DB=%-4.2f\n", (adc_read_a1()*100)+4);
   printf("\n hejsan \n");
-  printf(" DB=%-4.2f\n", ((double)read_noise_value()));
+  printf(" DB=%-4.2f\n", ((double)noise-sensor.read_noise_value()));
 
 }
 
