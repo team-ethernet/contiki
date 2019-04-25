884b8db2b0e153a9f62d398602f47b4938950694
Andreas <andro.sjodin@gmail.com>
2019-04-25 08:12:30 +0200

    adding comments to noise-sensor.c

--- platform/avr-rss2/dev/noise-sensor.c
+++ platform/avr-rss2/dev/noise-sensor.c
@@ -7,7 +7,19 @@
 #include "rss2.h"
 #include "dev/noise-sensor.h"
 
+/*	PROJECT: Kth 2019 Internet of Noisy things
+*	
+*	
+*	
+*	This file contains the driver for the external noise sensor
+*	Digital sound level meter version 1358-EN-00
+*	
+
+*/	
+
 double read_noise_value(void)
 {
-  return ((adc_read_a1()*100)+4);
+  return ((adc_read_a1()*100)); //Multiply voltage value with
+								//100 as the voltage is linearly proportional 
+								//with the dB value but 100 times smaller
 }
