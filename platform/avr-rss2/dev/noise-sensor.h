8a60e2d2f4c1d81c146df1bcba5a5738b39948a1
Andreas <andro.sjodin@gmail.com>
2019-04-25 09:40:52 +0200

    trying struct

--- platform/avr-rss2/dev/noise-sensor.h
+++ platform/avr-rss2/dev/noise-sensor.h
@@ -1,5 +1,7 @@
-#ifndef NOISE_SENSOR_H
-#define NOISE_SENSOR_H
+#ifndef NOISE_SENSOR_H_
+#define NOISE_SENSOR_H_
+
+#include "lib/sensors.h"
 
 double read_noise_value(void);
 extern const struct sensors_sensor noise_sensor;
