5f8f484eac0c43345b05d4296ff4fea3d2388d47
Erik Flink <erikfli@kth.se>
2019-05-07 10:49:34 +0200

    Fix errors

--- examples/avr-rss2/senml-api/senml-api.h
+++ examples/avr-rss2/senml-api/senml-api.h
@@ -2,7 +2,7 @@
 #define SENML_API_H_
 #include "label.h"
 
-void init_pack(char * buffer_pointer, int size);
-void end_pack();
+void init_senml(char * buffer_pointer, int size);
+void end_senml();
 void add_record(Label label, ...);
 #endif
