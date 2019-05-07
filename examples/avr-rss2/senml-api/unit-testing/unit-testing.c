a5d35ad9d966c7f4f09637c55d1e9695af355f3d
Valter <valterlu@kth.se>
2019-05-07 15:59:14 +0200

    Added tests for json and cbor

--- examples/avr-rss2/senml-api/unit-testing/unit-testing.c
+++ examples/avr-rss2/senml-api/unit-testing/unit-testing.c
@@ -3,8 +3,12 @@
 
 UNIT_TEST_REGISTER(json_empty_string, "json empty string");
 UNIT_TEST_REGISTER(json_noise_sensor, "json noise sensor example");
+UNIT_TEST_REGISTER(json_many_parameters, "json many parameters");
+UNIT_TEST_REGISTER(json_multiple_records, "json many records");
 UNIT_TEST_REGISTER(cbor_empty_string, "cbor empty string");
 UNIT_TEST_REGISTER(cbor_noise_sensor, "cbor noise sensor example");
+UNIT_TEST_REGISTER(cbor_many_paramters, "cbor many parameters");
+
 
 PROCESS(unit_testing, "Unit Testing");
 AUTOSTART_PROCESSES(&test_process);
@@ -15,8 +19,12 @@ PROCESS_THREAD(unit_testing, ev, data)
 
   UNIT_TEST_RUN(json_empty_string);
   UNIT_TEST_RUN(json_noise_sensor);
+  UNIT_TEST_RUN(json_many_parameters);
+  UNIT_TEST_RUN(json_multiple_records);
   UNIT_TEST_RUN(cbor_empty_string);
   UNIT_TEST_RUN(cbor_noise_sensor);
+  UNIT_TEST_RUN(cbor_many_parameters);
+
 
   PROCESS_END();
 }
@@ -49,6 +57,31 @@ Test(json_noise_sensor) {
   UNIT_TEST_END();
 }
 
+Test(json_many_parameters){
+  UNIT_TEST_BEGIN();
+
+  init_senml(buffer_pointer, 1024);
+  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", BASE_TIME, 123456789, BASE_UNIT, "Volt", BASE_VERSION, 2, VALUE, -1, SUM, 0, TIME, 643, UPDATE_TIME, 000, NULL);
+  end_senml();
+
+  UNIT_TEST_ASSERT(*buffer_pointer == "[{\"bn\":\"urn:dev:mac:fcc23d0000003790\",\"bt\":123456789,\"bu\":\"Volt\",\"bv\":2,\"v\":-1,\"s\":0,\"t\":643,\"tu\":000}]")
+
+}
+
+Test(json_multiple_records) {
+  UNIT_TEST_BEGIN();
+
+  init_senml(buffer_pointer, 1024);
+  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", UNIT, "dB", VALUE, 50.00, NULL);
+  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003788", UNIT, "w", VALUE, 0.00, NULL);
+  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000013290", UNIT, "m", VALUE, 35.25, NULL);
+  end_senml();
+
+  UNIT_TEST_ASSERT(*buffer_pointer == "[{\"bn\":\"urn:dev:mac:fcc23d0000003790\",\"u\":\"dB\",\"v\":50.00},{\"bn\":\"urn:dev:mac:fcc23d0000003788\",\"u\":\"w\",\"v\":0.00},{\"bn\":\"fcc23d0000013290\",\"u\":\"m\",\"v\":35.25}]");
+
+  UNIT_TEST_END();
+}
+
 Test(cbor_empty_string){
   UNIT_TEST_BEGIN();
 
@@ -73,4 +106,13 @@ Test(cbor_noise_sensor){
   UNIT_TEST_END();
 }
 
-//Add test cases
+Test(cbor_many_paramters){
+  UNIT_TEST_BEGIN();
+
+  init_senml(buffer_pointer, 1024);
+  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", BASE_TIME, 123456789, BASE_UNIT, "Volt", BASE_VERSION, 2, VALUE, -1, SUM, 0, TIME, 643, UPDATE_TIME, 000, NULL);
+  end_senml();
+  UNIT_TEST_ASSERT(*buffer_pointer == "81A862626E781C75726E3A6465763A6D61633A666363323364303030303030333739306262741A075BCD1562627564566F6C74646276657202617620617300617419028362757400")
+
+  UNIT_TEST_END();
+}
