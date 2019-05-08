df501a888f7f9d42ba12adc676f8dc7842b9acc6
Isak <isaak.olsson@gmail.com>
2019-05-08 11:41:56 +0200

    added cbor multiple records

--- apps/senml/unit-testing/unit-testing.c
+++ apps/senml/unit-testing/unit-testing.c
@@ -8,6 +8,7 @@ UNIT_TEST_REGISTER(json_multiple_records, "json many records");
 UNIT_TEST_REGISTER(cbor_empty_string, "cbor empty string");
 UNIT_TEST_REGISTER(cbor_noise_sensor, "cbor noise sensor example");
 UNIT_TEST_REGISTER(cbor_many_parameters, "cbor many parameters");
+UNIT_TEST_REGISTER(cbor_multiple_records, "cbor multiple records");
 
 
 PROCESS(unit_testing, "Unit Testing");
@@ -23,6 +24,7 @@ PROCESS_THREAD(unit_testing, ev, data){
   UNIT_TEST_RUN(cbor_empty_string);
   UNIT_TEST_RUN(cbor_noise_sensor);
   UNIT_TEST_RUN(cbor_many_parameters);
+  UNIT_TEST_RUN(cbor_multiple_records);
 
   PROCESS_END();
 }
@@ -111,6 +113,20 @@ Test(cbor_many_paramters){
   add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", BASE_TIME, 123456789, BASE_UNIT, "Volt", BASE_VERSION, 2, VALUE, -1, SUM, 0, TIME, 643, UPDATE_TIME, 000, NULL);
   end_senml();
   UNIT_TEST_ASSERT(*buffer_pointer == "81A862626E781C75726E3A6465763A6D61633A666363323364303030303030333739306262741A075BCD1562627564566F6C74646276657202617620617300617419028362757400")
+
+  UNIT_TEST_END();
+}
+
+Test(cbor_multiple_records) {
+  UNIT_TEST_BEGIN();
+
+  init_senml(buffer_pointer, 1024);
+  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", UNIT, "dB", VALUE, 50.00, NULL);
+  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003788", UNIT, "w", VALUE, 0.00, NULL);
+  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000013290", UNIT, "m", VALUE, 35.25, NULL);
+  end_senml();
+
+  UNIT_TEST_ASSERT(*buffer_pointer == "83A362626E781C75726E3A6465763A6D61633A6663633233643030303030303337393061756264426176F95240A362626E781C75726E3A6465763A6D61633A66636332336430303030303033373838617561776176F90000A362626E70666363323364303030303031333239306175616D6176F95068");
 
   UNIT_TEST_END();
 }
