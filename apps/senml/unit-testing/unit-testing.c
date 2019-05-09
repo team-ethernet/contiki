1abb7797c62c738f5b8e758bfe14d1f9491301e9
Anton Bothin <abothin@kth.se>
2019-05-09 10:53:38 +0200

    Include SenML API and uses macros

--- apps/senml/unit-testing/unit-testing.c
+++ apps/senml/unit-testing/unit-testing.c
@@ -1,5 +1,7 @@
 #include "contiki.h"
 #include "unit-test.h"
+#include "../senml-json.h"
+#include <string.h>
 #include <stdio.h>
 
 void
@@ -27,107 +29,108 @@ UNIT_TEST_REGISTER(cbor_multiple_records, "cbor multiple records");
 
 //All json tests should be run wiht json file included
 UNIT_TEST(json_empty_string){
-char * buffer_pointer[1024];
+char buffer_pointer[1024];
   UNIT_TEST_BEGIN();
 
-  init_senml(buffer_pointer, 1024);
-  end_senml();
+  INIT_SENML_JSON(buffer_pointer, 1024);
+  END_SENML();
 
-  UNIT_TEST_ASSERT(*buffer_pointer == "[]");
+  UNIT_TEST_ASSERT(buffer_pointer == "[]");
 
   UNIT_TEST_END();
 }
 
 
 UNIT_TEST(json_noise_sensor){
-char * buffer_pointer[1024];
+char buffer_pointer[1024];
   UNIT_TEST_BEGIN();
 
-  init_senml(buffer_pointer, 1024);
-  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", UNIT, "dB", VALUE, 50.00, NULL);
-  end_senml();
+  INIT_SENML_JSON(buffer_pointer, 1024);
+  ADD_RECORD(BASE_NAME, "urn:dev:mac:fcc23d0000003790", UNIT, "dB", VALUE, 50.00, NULL);
+  END_SENML();
 
-  UNIT_TEST_ASSERT(*buffer_pointer == "[{\"bn\":\"urn:dev:mac:fcc23d0000003790\",\"u\":\"dB\",\"v\":50.00}]");
+  UNIT_TEST_ASSERT(buffer_pointer == "[{\"bn\":\"urn:dev:mac:fcc23d0000003790\",\"u\":\"dB\",\"v\":50.00}]");
 
   UNIT_TEST_END();
 }
 
 UNIT_TEST(json_many_parameters){
-char * buffer_pointer[1024];
+char buffer_pointer[1024];
   UNIT_TEST_BEGIN();
 
-  init_senml(buffer_pointer, 1024);
-  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", BASE_TIME, 123456789, BASE_UNIT, "Volt", BASE_VERSION, 2, VALUE, -1, SUM, 0, TIME, 643, UPDATE_TIME, 000, NULL);
-  end_senml();
+  INIT_SENML_JSON(buffer_pointer, 1024);
+  ADD_RECORD(BASE_NAME, "urn:dev:mac:fcc23d0000003790", BASE_TIME, 123456789, BASE_UNIT, "Volt", BASE_VERSION, 2, VALUE, -1, SUM, 0, TIME, 643, UPDATE_TIME, 000, NULL);
+  END_SENML();
 
-  UNIT_TEST_ASSERT(*buffer_pointer == "[{\"bn\":\"urn:dev:mac:fcc23d0000003790\",\"bt\":123456789,\"bu\":\"Volt\",\"bv\":2,\"v\":-1,\"s\":0,\"t\":643,\"tu\":000}]");
+  UNIT_TEST_ASSERT(buffer_pointer == "[{\"bn\":\"urn:dev:mac:fcc23d0000003790\",\"bt\":123456789,\"bu\":\"Volt\",\"bv\":2,\"v\":-1,\"s\":0,\"t\":643,\"tu\":000}]");
 
+  UNIT_TEST_END()
 }
 
 UNIT_TEST(json_multiple_records){
-char * buffer_pointer[1024];
+char buffer_pointer[1024];
   UNIT_TEST_BEGIN();
 
-  init_senml(buffer_pointer, 1024);
-  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", UNIT, "dB", VALUE, 50.00, NULL);
-  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003788", UNIT, "w", VALUE, 0.00, NULL);
-  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000013290", UNIT, "m", VALUE, 35.25, NULL);
-  end_senml();
+  INIT_SENML_JSON(buffer_pointer, 1024);
+  ADD_RECORD(BASE_NAME, "urn:dev:mac:fcc23d0000003790", UNIT, "dB", VALUE, 50.00, NULL);
+  ADD_RECORD(BASE_NAME, "urn:dev:mac:fcc23d0000003788", UNIT, "w", VALUE, 0.00, NULL);
+  ADD_RECORD(BASE_NAME, "urn:dev:mac:fcc23d0000013290", UNIT, "m", VALUE, 35.25, NULL);
+  END_SENML();
 
-  UNIT_TEST_ASSERT(*buffer_pointer == "[{\"bn\":\"urn:dev:mac:fcc23d0000003790\",\"u\":\"dB\",\"v\":50.00},{\"bn\":\"urn:dev:mac:fcc23d0000003788\",\"u\":\"w\",\"v\":0.00},{\"bn\":\"fcc23d0000013290\",\"u\":\"m\",\"v\":35.25}]");
+  UNIT_TEST_ASSERT(buffer_pointer == "[{\"bn\":\"urn:dev:mac:fcc23d0000003790\",\"u\":\"dB\",\"v\":50.00},{\"bn\":\"urn:dev:mac:fcc23d0000003788\",\"u\":\"w\",\"v\":0.00},{\"bn\":\"fcc23d0000013290\",\"u\":\"m\",\"v\":35.25}]");
 
   UNIT_TEST_END();
 }
 
 UNIT_TEST(cbor_empty_string){
-char * buffer_pointer[1024];
+char buffer_pointer[1024];
   UNIT_TEST_BEGIN();
 
-  init_senml(buffer_pointer, 1024);
-  end_senml();
+  INIT_SENML_CBOR(buffer_pointer, 1024);
+  END_SENML();
 
-  UNIT_TEST_ASSERT(*buffer_pointer == "80");
+  UNIT_TEST_ASSERT(buffer_pointer == "80");
 
   UNIT_TEST_END();
 
 }
 
 UNIT_TEST(cbor_noise_sensor){
-char * buffer_pointer[1024];
+char buffer_pointer[1024];
   UNIT_TEST_BEGIN();
 
-  init_senml(buffer_pointer, 1024);
-  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", UNIT, "dB", VALUE, 50.00, NULL);
-  end_senml();
+  INIT_SENML_CBOR(buffer_pointer, 1024);
+  ADD_RECORD(BASE_NAME, "urn:dev:mac:fcc23d0000003790", UNIT, "dB", VALUE, 50.00, NULL);
+  END_SENML();
 
-  UNIT_TEST_ASSERT(*buffer_pointer == "81A362626E781C75726E3A6465763A6D61633A6663633233643030303030303337393061756264426176F95240");
+  UNIT_TEST_ASSERT(buffer_pointer == "81A362626E781C75726E3A6465763A6D61633A6663633233643030303030303337393061756264426176F95240");
 
   UNIT_TEST_END();
 }
 
 UNIT_TEST(cbor_many_paramters){
-char * buffer_pointer[1024];
+char buffer_pointer[1024];
   UNIT_TEST_BEGIN();
 
-  init_senml(buffer_pointer, 1024);
-  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", BASE_TIME, 123456789, BASE_UNIT, "Volt", BASE_VERSION, 2, VALUE, -1, SUM, 0, TIME, 643, UPDATE_TIME, 000, NULL);
-  end_senml();
-  UNIT_TEST_ASSERT(*buffer_pointer == "81A862626E781C75726E3A6465763A6D61633A666363323364303030303030333739306262741A075BCD1562627564566F6C74646276657202617620617300617419028362757400");
+  INIT_SENML_CBOR(buffer_pointer, 1024);
+  ADD_RECORD(BASE_NAME, "urn:dev:mac:fcc23d0000003790", BASE_TIME, 123456789, BASE_UNIT, "Volt", BASE_VERSION, 2, VALUE, -1, SUM, 0, TIME, 643, UPDATE_TIME, 000, NULL);
+  END_SENML();
+  UNIT_TEST_ASSERT(buffer_pointer == "81A862626E781C75726E3A6465763A6D61633A666363323364303030303030333739306262741A075BCD1562627564566F6C74646276657202617620617300617419028362757400");
 
   UNIT_TEST_END();
 }
 
 UNIT_TEST(cbor_multiple_records) {
-char * buffer_pointer[1024];
+char buffer_pointer[1024];
   UNIT_TEST_BEGIN();
 
-  init_senml(buffer_pointer, 1024);
-  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003790", UNIT, "dB", VALUE, 50.00, NULL);
-  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000003788", UNIT, "w", VALUE, 0.00, NULL);
-  add_record(BASE_NAME, "urn:dev:mac:fcc23d0000013290", UNIT, "m", VALUE, 35.25, NULL);
-  end_senml();
+  INIT_SENML_CBOR(buffer_pointer, 1024);
+  ADD_RECORD(BASE_NAME, "urn:dev:mac:fcc23d0000003790", UNIT, "dB", VALUE, 50.00, NULL);
+  ADD_RECORD(BASE_NAME, "urn:dev:mac:fcc23d0000003788", UNIT, "w", VALUE, 0.00, NULL);
+  ADD_RECORD(BASE_NAME, "urn:dev:mac:fcc23d0000013290", UNIT, "m", VALUE, 35.25, NULL);
+  END_SENML();
 
-  UNIT_TEST_ASSERT(*buffer_pointer == "83A362626E781C75726E3A6465763A6D61633A6663633233643030303030303337393061756264426176F95240A362626E781C75726E3A6465763A6D61633A66636332336430303030303033373838617561776176F90000A362626E70666363323364303030303031333239306175616D6176F95068");
+  UNIT_TEST_ASSERT(buffer_pointer == "83A362626E781C75726E3A6465763A6D61633A6663633233643030303030303337393061756264426176F95240A362626E781C75726E3A6465763A6D61633A66636332336430303030303033373838617561776176F90000A362626E70666363323364303030303031333239306175616D6176F95068");
 
   UNIT_TEST_END();
 }
